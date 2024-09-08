802e4a2217ed552538508a904d25958cabf0bc9f
lmhy220103 <leminhhy2212003@gmail.com>
2024-09-08 20:43:27 +0700

    finished

---
+++ src/pages/client/Carousel.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef } from "react";
+import Slider from "react-slick";
+
+const SimpleSlider = (props) => {
+  const ref = useRef({});
+
+  const next = () => {
+    ref.current.slickNext();
+  };
+
+  const previous = () => {
+    ref.current.slickPrev();
+  };
+
+  const settings = {
+    className: "section-outstanding__slider",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    rows: 2,
+    responsive: [
+      {
+        breakpoint: 1198,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          rows: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Slider ref={ref} {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+  );
+};
+
+export default SimpleSlider;
--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -3,17 +3,25 @@ import styled from "styled-components";
 import { Banner } from "./Banner";
 import Courses from "./Courses";
 import { Info } from "./Info";
+import CarouselSlide from "./Carousel";
+import { Bottom } from "./Bottom";
 
 export const Home = () => {
   return (
     <>
       <Container>
-        <Banner/>
-        <Info/>
+        <Banner />
+        <Info />
         <div className="flex justify-between mt-20">
-          <h6><a className="font-600 text-yellow-400" href="">Khóa học phổ biến</a></h6>
+          <h6>
+            <a className="font-600 text-yellow-400" href="">
+              Khóa học phổ biến
+            </a>
+          </h6>
         </div>
-        <Courses/>
+        <Courses />
+        <CarouselSlide />
+        <Bottom />
       </Container>
     </>
   );
